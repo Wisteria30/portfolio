53820854205493ac5c85db93e8eb300dce6251e6
wisteria30 <wis30@wisteria30.local>
2019-05-15 23:57:11 +0900

    Updates

---
+++ precache-manifest.d769b666324b24e36a9ede35cce3cc42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a2a897f4a47550da75a9ebb0139a35",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d09f286e5ac4ba94d570",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f6f8e08832a73ed318a8",
+    "url": "/portfolio/static/js/2.56242e93.chunk.js"
+  },
+  {
+    "revision": "d09f286e5ac4ba94d570",
+    "url": "/portfolio/static/js/main.bddf58d6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
